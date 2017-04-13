5db24634581d10fed39e9aa87907670304cd9d16
Elliot Young <elliotsyoung@gmail.com>
2017-04-13 14:12:10 -0700

    updated code with more card sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
-  return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
-  );
+    return (
+      <Card>
+        <CardSection>
+          <Text>{props.album.title}</Text>
+        </CardSection>
+        <CardSection>
+          <Text>{props.album.title}</Text>
+        </CardSection>
+      </Card>
+    );
 };
 
 export default AlbumDetail;
