614edc70d1884d1458867a08f3b1424b7a83bbe0
Elliot Young <elliotsyoung@gmail.com>
2017-04-13 13:44:22 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,18 @@
+// Index.ios.js place code in here for iOS
+
+// Import a library to help create a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header.js';
+import AlbumList from './src/components/AlbumList.js';
+
+// Create a Component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('TestProject', () => App);
